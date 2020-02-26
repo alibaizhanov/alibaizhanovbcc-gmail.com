07e12130f167766d9c1ccca655c21d20b2fd38f8
a.baizhanov <a.baizhanov@p-s.kz>
2020-02-26 19:19:55 +0600

    svg add

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,32 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import ruLocale from 'date-fns/locale/ru';
+import classNames from 'classnames';
+//import checkedSvg from 'assests/img/readed.svg';
 
 import './Message.scss';
 
 
 
-const Message = ({avatar,user,text,date}) => (
-     <div className = "message">
-
-       <div className = "message__avatar">
-
-          <img src = {avatar} alt={`Avatar ${user.fullname}`}/>
-
+const Message = ({avatar,user,text,date,isMe}) => (
+     <div className={classNames("message",{'message--isme':isMe})}>
+       <div className="message__content">
+          <div className="message__avatar">
+              <img src={avatar} alt={`Avatar ${user.fullname}`}/>
+          </div>
+          <div className="message__info">
+              <div className="message__bubble">
+                    <p className="message__text">{text}</p>
+              </div>
+              <span className="message__date"> 
+                  {formatDistanceToNow(new Date(date),'ddd/MMM/D/YYYY/hh/m/ss',{includeSeconds:true,addSuffix:true,locale:ruLocale})}
+              </span>
+          </div>
        </div>
-
-       <div className = "message__content">
-
-        <div className = "message__bubble">
-
-              <p className= "message__text">{text}</p>
-
-        </div>
-
-          <span className = "message__date"> {formatDistanceToNow(new Date(date),'ddd/MMM/D/YYYY/hh/m/ss',{includeSeconds:true,addSuffix:true,locale:ruLocale})}</span>
-
-       </div>
-
      </div>
 );
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,8 +9,14 @@ const Home = () => (
 
              <Message 
                 avatar = "https://sun9-35.userapi.com/c627724/v627724488/c494/j2lafv0ysJ0.jpg?ava=1" 
-                text = "Hello" 
+                text = "Добрый день , не получается сделать время отправки так же , как и в видео. В консоле пишет , что модулю не найден. Попробовал сделать formatDistanceToNow на странице выводится RangeError: Invalid time value" 
                 date='Wed Feb 12 2020 16:54:20'/> 
+             <Message 
+                avatar = "https://sun9-35.userapi.com/c627724/v627724488/c494/j2lafv0ysJ0.jpg?ava=1" 
+                text = "Добрый день , не получается сделать время отправки так же , как и в видео. В консоле пишет , что модулю не найден. Попробовал сделать formatDistanceToNow на странице выводится RangeError: Invalid time value" 
+                date='Wed Feb 26 2020 15:54:20'
+                isMe={true}
+                isReaded={true}/>
             
         </section>
     );
