70ba24b68e877c568c3383fdc37dd6404d1ab39c
a.baizhanov <a.baizhanov@p-s.kz>
2020-04-16 16:43:11 +0600

    Верстка

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ const Home = () => (
 
              <Message 
                 avatar = "https://sun9-35.userapi.com/c627724/v627724488/c494/j2lafv0ysJ0.jpg?ava=1" 
-                text = "Добрый день , не получается сделать время отправки так же , как и в видео. В консоле пишет , что модулю не найден. Попробовал сделать formatDistanceToNow на странице выводится RangeError: Invalid time value" 
+                text = "Добрый день , не получается сделать время отправки. В консоле пишет , что модулю не найден. Попробовал сделать formatDistanceToNow на странице выводится RangeError: Invalid time value" 
                 date='Wed Feb 12 2020 16:54:20'
                 attachments={[
                     {
@@ -27,7 +27,7 @@ const Home = () => (
                 ]}/> 
              <Message 
                 avatar = "https://sun9-35.userapi.com/c627724/v627724488/c494/j2lafv0ysJ0.jpg?ava=1" 
-                text = "Добрый день , не получается сделать время отправки так же , как и в видео. В консоле пишет , что модулю не найден. Попробовал сделать formatDistanceToNow на странице выводится RangeError: Invalid time value" 
+                text = "Добрый день , не получается сделать время отправки. В консоле пишет , что модулю не найден. Попробовал сделать formatDistanceToNow на странице выводится RangeError: Invalid time value" 
                 date='Wed Feb 26 2020 15:54:20'
                 isMe={true}
                 isReaded={false}
